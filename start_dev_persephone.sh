fbfc02aed5ca06c08b18883b0a45b2258a58c0d1
secret <secretisdead@gmail.com>
2019-02-02 15:36:21 -0800

    add detection for alternate python binary/alias name in start dev server

--- start_dev_persephone.sh
+++ start_dev_persephone.sh
@@ -3,7 +3,12 @@
 persephone=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 cd "${persephone}"
 
+python=python3
+${python} --version || (
+	python=python
+)
+
 source ./environment/bin/activate
 export FLASK_DEBUG=ON
 export FLASK_APP=./persephone_wsgi.py
-python3 -m flask run --host 0.0.0.0 --port 5000
+${python} -m flask run --host 0.0.0.0 --port 5000
