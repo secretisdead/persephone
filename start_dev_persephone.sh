7cba5884656d7b0bd4183ec96416cc87cd4cb355
secret <secretisdead@gmail.com>
2019-09-02 07:45:15 -0700

    ensure dev server python env language is utf-8

--- start_dev_persephone.sh
+++ start_dev_persephone.sh
@@ -12,4 +12,6 @@ source ./environment/bin/activate
 export FLASK_DEBUG=ON
 export FLASK_APP=./persephone_wsgi.py
 export PERSEPHONE_DIR=${persephone}
+export LC_ALL=C.UTF-8
+export LANG=C.UTF-8
 ${python} -m flask run --host 0.0.0.0 --port 5000
